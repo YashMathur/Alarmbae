2222376e00bbe00af2c615516e6ce446e4b8603f
Yash Mathur <yash97mathur@gmail.com>
2016-11-26 14:35:21 -0500

    update memeframe

--- app/src/main/java/alarmbae/terrible/hacks/alarmbae/MainActivity.java
+++ app/src/main/java/alarmbae/terrible/hacks/alarmbae/MainActivity.java
@@ -1,6 +1,6 @@
 package alarmbae.terrible.hacks.alarmbae;
 
-import android.support.v7.app.AppCompatActivity;
+import android.opengl.GLES20;
 import android.os.Bundle;
 
 import com.google.vr.sdk.base.Eye;
@@ -9,10 +9,18 @@ import com.google.vr.sdk.base.GvrView;
 import com.google.vr.sdk.base.HeadTransform;
 import com.google.vr.sdk.base.Viewport;
 
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
+import java.nio.FloatBuffer;
+import java.nio.ShortBuffer;
+
 import javax.microedition.khronos.egl.EGLConfig;
 
-public class MainActivity extends GvrActivity implements GvrView.StereoRenderer {
+import static alarmbae.terrible.hacks.alarmbae.MemeFrame.squareCoords;
 
+public class MainActivity extends GvrActivity implements GvrView.StereoRenderer {
+    private FloatBuffer vertexBuffer;
+    private ShortBuffer drawListBuffer;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -41,7 +49,36 @@ public class MainActivity extends GvrActivity implements GvrView.StereoRenderer 
 
     @Override
     public void onSurfaceCreated(EGLConfig eglConfig) {
+        ByteBuffer bb = ByteBuffer.allocateDirect(MemeFrame.squareCoords.length * 4); // (# of coordinate values * 4 bytes per float)
+        bb.order(ByteOrder.nativeOrder());
+        vertexBuffer = bb.asFloatBuffer();
+        vertexBuffer.put(MemeFrame.squareCoords);
+        vertexBuffer.position(0);
 
+        // initialize byte buffer for the draw list
+        ByteBuffer dlb = ByteBuffer.allocateDirect(MemeFrame.drawOrder.length * 2); // (# of coordinate values * 2 bytes per short)
+        dlb.order(ByteOrder.nativeOrder());
+        drawListBuffer = dlb.asShortBuffer();
+        drawListBuffer.put(MemeFrame.drawOrder);
+        drawListBuffer.position(0);
+
+        int vertexShader = loadShader(GLES20.GL_VERTEX_SHADER, MemeFrame.vertexShaderCode);
+        int fragmentShader = loadShader(GLES20.GL_FRAGMENT_SHADER, MemeFrame.fragmentShaderCode);
+
+
+    }
+
+    public static int loadShader(int type, String shaderCode){
+
+        // create a vertex shader type (GLES20.GL_VERTEX_SHADER)
+        // or a fragment shader type (GLES20.GL_FRAGMENT_SHADER)
+        int shader = GLES20.glCreateShader(type);
+
+        // add the source code to the shader and compile it
+        GLES20.glShaderSource(shader, shaderCode);
+        GLES20.glCompileShader(shader);
+
+        return shader;
     }
 
     @Override
--- app/src/main/java/alarmbae/terrible/hacks/alarmbae/MemeFrame.java
+++ app/src/main/java/alarmbae/terrible/hacks/alarmbae/MemeFrame.java
@@ -5,4 +5,28 @@ package alarmbae.terrible.hacks.alarmbae;
  */
 
 public class MemeFrame {
+    static final String vertexShaderCode =
+            "attribute vec4 vPosition;" +
+                    "void main() {" +
+                    "  gl_Position = vPosition;" +
+                    "}";
+
+    static final String fragmentShaderCode =
+            "precision mediump float;" +
+                    "uniform vec4 vColor;" +
+                    "void main() {" +
+                    "  gl_FragColor = vColor;" +
+                    "}";
+
+    static float squareCoords[] = { -0.5f,  0.5f, 0.0f,   // top left
+            -0.5f, -0.5f, 0.0f,   // bottom left
+            0.5f, -0.5f, 0.0f,   // bottom right
+            0.5f,  0.5f, 0.0f }; // top right
+
+    static short drawOrder[] = { 0, 1, 2, 0, 2, 3 }; // order to draw vertices
+    float color[] = { 0.63671875f, 0.76953125f, 0.22265625f, 1.0f };
+    private static int COORDS_PER_VERTEX = 3;
+    static final int vertexStride = COORDS_PER_VERTEX * 4;
+    static final int vertexCount = 4;
+
 }
